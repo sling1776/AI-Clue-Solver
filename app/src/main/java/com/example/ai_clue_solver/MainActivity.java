f3c31c3d8bd888fae8e2f57d2f05170a3623e11c
Spencer Lingwall <87142176+sling1776@users.noreply.github.com>
2022-05-10 15:07:43 -0600

    Adding Android Studio Project Files

---
+++ app/src/main/java/com/example/ai_clue_solver/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ai_clue_solver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Hello World!");
+    }
+}
